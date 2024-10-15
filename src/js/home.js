f0582e9812c09c4d6e2cc3c9600be56a5bcdc071
mrgnschndr <mrgnschndr@gmail.com>
2024-10-14 21:28:21 -0600

    fix: file names in html meta tags

---
+++ src/js/home.js
@@ -0,0 +1,6 @@
+// #4 - Form for User's Name
+
+let userName = document.getElementById('userName');
+let adoptBtn = document.getElementsByTagName('button');
+console.log(userName);
+console.log(adoptBtn);
