c7ed6733c1dbfdafa4d32c00f465db9d4c7d910c
mrgnschndr <mrgnschndr@gmail.com>
2024-10-15 13:24:38 -0600

    wip modal display work

--- src/js/home.js
+++ src/js/home.js
@@ -1,9 +1,12 @@
 // #4 - Form for User's Name
-
+// Set up modal display
 let userName = document.getElementById('userName');
-let adoptBtn = document.getElementsByTagName('button');
+let adoptBtn = document.getElementbyId('adoptBtn');
 let adoptModal = document.getElementById('adoptModal');
+let closeBtn = document.getElementById('closeBtn');
+let petName = document.createElement('input');
 
+//Set up modal actions
 adoptBtn.addEventListener('click', function() {
     adoptModal.style.display = "block";
-})
+});
