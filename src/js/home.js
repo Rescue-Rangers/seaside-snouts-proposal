fff9c6c3a0bc637f1502ec77c58ca7a1831d1712
mrgnschndr <mrgnschndr@gmail.com>
2024-10-16 12:55:00 -0600

    Finish ticket #4 Form for User's Name

--- src/js/home.js
+++ src/js/home.js
@@ -1,32 +1,63 @@
-// #4 - Form for User's Name
-// Set up modal display
+//#4 - Form for User's Name
+//Set up modal display
 let userName = document.getElementById('userName');
 let adoptBtn = document.getElementById('adoptBtn');
 let adoptModal = document.getElementById('adoptModal');
 let closeBtn = document.getElementById('closeBtn');
 let modalContent = document.getElementById('adoptSuccess');
+let petNameBtn = document.getElementById('petNameBtn');
 let pet = "";
 
 //Set up modal actions
+// Validate and capture user's name, then open next modal
 adoptBtn.addEventListener('click', function() {
     let userNameInput = document.getElementById('userName').value;
-    if (!isNaN(userNameInput) || userNameInput.length <= 1) {
-        window.alert('Invalid name');
+    if (!userNameInput) {
+        window.alert('Please enter your name.');
+    } else if (!checkForNumChar(userNameInput)) {
+        window.alert('Please use alphabetical numbers only.');
+    } else if (userNameInput.length == 1) {
+        window.alert('Hmm... That looks like an initial. Please enter your full name.');
+    } else if (userNameInput.length > 16) {
+        window.alert('Please use 16 characters or less.');
     } else {
         modalContent.innerHTML = `Congrats, ${userNameInput}! You have adopted a ${pet}! What would you like to name them?`;
+        document.getElementById('userName').value = "";
         adoptModal.style.display = "block";
+        return userNameInput;
     }
 });
 
+// // Add close button functionality
+// closeBtn.addEventListener('click', function() {
+//     adoptModal.style.display = "none";
+// })
 
-closeBtn.addEventListener('click', function() {
-    adoptModal.style.display = "none";
-})
+// // Validate and capture pet's name, then navigate user to Game page
+// petNameBtn.addEventListener('click', function() {
+//     let petNameInput = document.getElementById('petName').value;
+//     if (!isNaN(petNameInput) || petNameInput.length <= 1) {
+//         window.alert('Invalid pet name');
+//     } else {
+//         window.location.assign('/src/html/game/index.html');
+//         return petNameInput;
+//     }
+// })
+
+
+function checkForNumChar(input) {
+    for (let i = 0; i < input.length; i++) {
+        let checkNum = input.charCodeAt(i);
+        if ((checkNum >= 65 && checkNum <= 90) || (checkNum >= 97 && checkNum <= 122) || (checkNum >= 45 && checkNum <= 46)) {
+            { continue; }
+        } else {
+            return false;
+        }
+    } return true;
+}
 
 
 
 
-
-// let petNameInput = document.getElementById('petName').value;
 
 
